ef53f4d9ff1eab7b7182f5f6c1b8518a22a17035
wuero007 <angelscp07@gmail.com>
2024-03-16 17:37:14 -0600

    cuarto commit

--- app/src/main/java/com/example/galeriapa/MainActivity2.java
+++ app/src/main/java/com/example/galeriapa/MainActivity2.java
@@ -1,24 +1,42 @@
 package com.example.galeriapa;
 
-import android.os.Bundle;
 
-import androidx.activity.EdgeToEdge;
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.core.graphics.Insets;
-import androidx.core.view.ViewCompat;
-import androidx.core.view.WindowInsetsCompat;
 
 public class MainActivity2 extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        EdgeToEdge.enable(this);
-        setContentView(R.layout.activity_main2);
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
-            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
-            return insets;
+        setContentView(R.layout.activity_main2); // Asegúrate de que este layout coincida con tu archivo XML
+
+        final EditText editTextUsuario = findViewById(R.id.editTextUsuario);
+        final EditText editTextContraseña = findViewById(R.id.editTextContraseña);
+        Button buttonIniciarSesion = findViewById(R.id.buttonIniciarSesion);
+
+        buttonIniciarSesion.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String usuario = editTextUsuario.getText().toString();
+                String contraseña = editTextContraseña.getText().toString();
+
+                if (!usuario.isEmpty() && !contraseña.isEmpty()) {
+
+                    Toast.makeText(MainActivity2.this, "Inicio de sesión exitoso", Toast.LENGTH_SHORT).show();
+
+                    // Inicia MainActivity
+                    Intent intent = new Intent(MainActivity2.this, MainActivity.class);
+                    startActivity(intent);
+                } else {
+                    Toast.makeText(MainActivity2.this, "Por favor, complete todos los campos", Toast.LENGTH_SHORT).show();
+                }
+            }
         });
     }
 }
