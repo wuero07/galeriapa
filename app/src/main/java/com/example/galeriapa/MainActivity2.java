2a964500972cbc46390bdd3e79dfb898143b1c6c
wuero007 <angelscp07@gmail.com>
2024-03-16 17:50:19 -0600

    cuarto commit

--- app/src/main/java/com/example/galeriapa/MainActivity2.java
+++ app/src/main/java/com/example/galeriapa/MainActivity2.java
@@ -1,6 +1,5 @@
 package com.example.galeriapa;
 
-
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
@@ -11,10 +10,13 @@ import androidx.appcompat.app.AppCompatActivity;
 
 public class MainActivity2 extends AppCompatActivity {
 
+    private static final String USUARIO_VALIDO = "Angel";
+    private static final String CONTRASEÑA_VALIDA = "2004";
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main2); // Asegúrate de que este layout coincida con tu archivo XML
+        setContentView(R.layout.activity_main2);
 
         final EditText editTextUsuario = findViewById(R.id.editTextUsuario);
         final EditText editTextContraseña = findViewById(R.id.editTextContraseña);
@@ -27,13 +29,19 @@ public class MainActivity2 extends AppCompatActivity {
                 String contraseña = editTextContraseña.getText().toString();
 
                 if (!usuario.isEmpty() && !contraseña.isEmpty()) {
-
-                    Toast.makeText(MainActivity2.this, "Inicio de sesión exitoso", Toast.LENGTH_SHORT).show();
-
-                    // Inicia MainActivity
-                    Intent intent = new Intent(MainActivity2.this, MainActivity.class);
-                    startActivity(intent);
+                    // Aquí se realiza la validación de usuario y contraseña
+                    if (usuario.equals(USUARIO_VALIDO) && contraseña.equals(CONTRASEÑA_VALIDA)) {
+                        // Si la validación es exitosa, se muestra un mensaje de inicio de sesión exitoso
+                        Toast.makeText(MainActivity2.this, "Inicio de sesión exitoso", Toast.LENGTH_SHORT).show();
+                        // Y se inicia MainActivity
+                        Intent intent = new Intent(MainActivity2.this, MainActivity.class);
+                        startActivity(intent);
+                    } else {
+                        // Si la validación falla, se muestra un mensaje de error
+                        Toast.makeText(MainActivity2.this, "Usuario o contraseña incorrectos", Toast.LENGTH_SHORT).show();
+                    }
                 } else {
+                    // Si alguno de los campos está vacío, se muestra un mensaje solicitando completar ambos campos
                     Toast.makeText(MainActivity2.this, "Por favor, complete todos los campos", Toast.LENGTH_SHORT).show();
                 }
             }
