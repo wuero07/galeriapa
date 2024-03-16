ba35fa1ead7a4a44226c7fb6820d425a9905de86
wuero007 <angelscp07@gmail.com>
2024-03-16 17:27:02 -0600

    segundo commit

--- app/src/main/java/com/example/galeriapa/MainActivity.java
+++ app/src/main/java/com/example/galeriapa/MainActivity.java
@@ -1,24 +1,58 @@
 package com.example.galeriapa;
 
-import android.os.Bundle;
 
-import androidx.activity.EdgeToEdge;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageView;
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.core.graphics.Insets;
-import androidx.core.view.ViewCompat;
-import androidx.core.view.WindowInsetsCompat;
 
 public class MainActivity extends AppCompatActivity {
+
+    private ImageView imageView;
+    private final int[] imageResources = new int[]{
+            R.drawable.imagen1,
+            R.drawable.imagen2,
+            R.drawable.imagen3,
+            R.drawable.imagen4,
+            R.drawable.imagen5,
+            R.drawable.imagen6,
+            R.drawable.imagen7
+    };
+    private int currentPosition = 0;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        EdgeToEdge.enable(this);
         setContentView(R.layout.activity_main);
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
-            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
-            return insets;
+
+        imageView = findViewById(R.id.imageView);
+        updateImage();
+
+        findViewById(R.id.btnSiguiente).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                currentPosition = (currentPosition + 1) % imageResources.length;
+                updateImage();
+            }
         });
+
+        findViewById(R.id.btnAtras).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                currentPosition = (currentPosition - 1 + imageResources.length) % imageResources.length;
+                updateImage();
+            }
+        });
+
+        findViewById(R.id.btnExit).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                finish();
+            }
+        });
+    }
+
+    private void updateImage() {
+        imageView.setImageResource(imageResources[currentPosition]);
     }
 }
